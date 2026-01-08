589d0d8f001cb150fa35556bfe2ce46b5d98df21
johnraivenolazo <johnraivenolazo@gmail.com>
2026-01-09 01:10:15 +0800

    setup env

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import { PORT } from './config/env.js'
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Servers running')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port http://localhost:${PORT}`)
+}
+);
+
+export default app;
---
+++ config/env.js
@@ -0,0 +1,7 @@
+import { config } from 'dotenv';
+
+const env = process.env.NODE_ENV || 'development'
+
+config({ path: `.env.${env}.local` })
+
+export const { PORT } = process.env
