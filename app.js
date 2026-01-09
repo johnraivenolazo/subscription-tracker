afe5af86a0415f70f2ee799d36349ba8579acbea
johnraivenolazo <johnraivenolazo@gmail.com>
2026-01-09 20:48:52 +0800

    add middleware

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from 'express'
 import { PORT } from './config/env.js'
 import { userRouter, authRouter, subscriptionRouter } from './routes/index.js'
 import connectToDatabase from './database/mongodb.js'
+import errorMiddleware from './middlewares/error.middleware.js'
+import cookieParser from 'cookie-parser'
 
 const app = express()
 
@@ -9,9 +11,15 @@ app.get('/', (req, res) => {
     res.send('Servers running')
 })
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 app.use('/auth', authRouter)
 app.use('/users', userRouter)
 app.use('/subscriptions', subscriptionRouter)
+
+app.use(errorMiddleware);
 
 app.listen(PORT, async () => {
     console.log(`Server running on port http://localhost:${PORT}`)
