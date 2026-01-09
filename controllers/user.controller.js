62da241b0d703519a8b6449dd5c56f52f7ab277e
johnraivenolazo <johnraivenolazo@gmail.com>
2026-01-09 23:27:48 +0800

    setup authorization

---
+++ controllers/user.controller.js
@@ -0,0 +1,28 @@
+import User from "../schemas/user.model.js";
+
+export const getUsers = async (req, res, next) => {
+    try {
+        const users = await User.find();
+        res.status(200).json({ success: true, data: users });
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const getUser = async (req, res, next) => {
+    try {
+        const user = await User.findById(req.params.id).select('-password');
+        res.status(200).json({ success: true, data: user });
+
+        if (!user) {
+            return next(new Error('User not found'));
+            error.statusCode = 404;
+            throw error;
+        }
+
+        res.status(200).json({ success: true, data: user });
+
+    } catch (error) {
+        next(error);
+    }
+}
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,14 +1,12 @@
 import { Router } from 'express'
+import { getUser, getUsers } from '../controllers/user.controller.js'
+import authorize from '../middlewares/auth.middleware.js'
 
 const userRouter = Router()
 
-userRouter.get('/', (req, res) => {
-    res.send({ title: "Get All Users" })
-})
+userRouter.get('/', getUsers)
 
-userRouter.get('/:id', (req, res) => {
-    res.send({ title: "Get User by ID" })
-})
+userRouter.get('/:id', authorize, getUser)
 
 userRouter.post('/', (req, res) => {
     res.send({ title: "CREATE User" })
